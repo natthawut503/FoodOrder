4b07c47f95e0de6069bf0ae998e94ef843ae99af
natthawut503 <natthawutk65@nu.ac.th>
2025-01-06 16:51:47 +0700

    finish

--- lib/FoodMenu.dart
+++ lib/FoodMenu.dart
@@ -15,6 +15,6 @@ List<FoodMenu> foodMenuList = [
   FoodMenu(name: "Salad", price: 80, ingredients: ["Lettuce", "Tomato", "Cheese"]),
   FoodMenu(name: "Steak", price: 200, ingredients: ["Beef", "Cheese", "Potato"]),
   FoodMenu(name: "Pasta", price: 100, ingredients: ["Pasta", "Cheese", "Beef"]),
-  FoodMenu(name: "Hamburger", price: 100, ingredients: ["Meat", "Bread", "Tomato","Cheese","Lettuce"]),
-  FoodMenu(name: "Frenchfries Cheese", price: 100, ingredients: ["Frenchfries", "Cheese"]),
+  FoodMenu(name: "Hamburger", price: 80, ingredients: ["Meat", "Bread", "Tomato","Cheese","Lettuce"]),
+  FoodMenu(name: "Frenchfries Cheese", price: 50, ingredients: ["Frenchfries", "Cheese"]),
 ];
